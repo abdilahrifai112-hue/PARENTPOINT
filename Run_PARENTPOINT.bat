9e7ad2a1d484a8bb4d6ce36d98eef20f036dda2c
RI PAY <abdilahrifai112@gmail.com>
2026-07-06 14:33:50 +0700

    Add runner

---
+++ Run_PARENTPOINT.bat
@@ -0,0 +1,11 @@
+@echo off
+color 0A
+echo ========================================================
+echo             MENJALANKAN PARENTPOINT...
+echo ========================================================
+echo Jika layar ini berhenti lama, berarti sedang mencoba
+echo menghubungi server database (Aiven Cloud)...
+echo Mohon tunggu maksimal 1 menit.
+echo ========================================================
+java -jar PARENTPOINT_SiapPakai.jar
+pause
