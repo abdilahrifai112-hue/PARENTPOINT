3bb2f08b113cfcf3772d17948bdd4ca07549e67f
RI PAY <abdilahrifai112@gmail.com>
2026-07-06 13:57:01 +0700

    Fix antivirus false positive by avoiding fat JAR

--- build.bat
+++ build.bat
@@ -4,36 +4,25 @@ cd C:\Users\HP\Documents\NetBeansProjects\PARENTPOINT
 echo === 1. Mengcompile Java Classes ===
 call compile_all.bat
 
-echo === 2. Membuat Standalone JAR ===
-rmdir /s /q temp_build 2>nul
-mkdir temp_build
+echo === 2. Membuat Standalone JAR (Aman dari Antivirus) ===
+rmdir /s /q APP_Release 2>nul
+mkdir APP_Release
+mkdir APP_Release\lib
 
-:: Copy resources (images, dll) dari src
-xcopy src\* temp_build\ /E /I /Y
+:: Copy library files
+xcopy lib\* APP_Release\lib\ /E /I /Y
+
+:: Create Manifest
+echo Main-Class: parentpoint.LOGIN.LOGIN> manifest_safe.txt
+echo Class-Path: lib/commons-beanutils-1.9.4.jar lib/commons-collections-3.2.2.jar lib/commons-collections4-4.4.jar lib/commons-digester-2.1.jar lib/commons-logging-1.2.jar lib/ecj-3.21.0.jar lib/jasperreports-6.20.6.jar lib/jcalendar-1.4.jar lib/mysql-connector-java-8.0.28.jar lib/sqlite-jdbc-3.41.2.2.jar>> manifest_safe.txt
+echo.>> manifest_safe.txt
 
 :: Copy compiled class files
-xcopy build\classes\* temp_build\ /E /I /Y
+xcopy src\parentpoint\images\*.* build\classes\parentpoint\images\ /Y /Q
 
-cd temp_build
-jar xf ..\lib\mysql-connector-java-8.0.28.jar
-jar xf ..\lib\sqlite-jdbc-3.41.2.2.jar
-jar xf ..\lib\jcalendar-1.4.jar
-jar xf ..\lib\commons-beanutils-1.9.4.jar
-jar xf ..\lib\commons-collections-3.2.2.jar
-jar xf ..\lib\commons-collections4-4.4.jar
-jar xf ..\lib\commons-digester-2.1.jar
-jar xf ..\lib\commons-logging-1.2.jar
-jar xf ..\lib\ecj-3.21.0.jar
-jar xf ..\lib\jasperreports-6.20.6.jar
-rmdir /s /q META-INF 2>nul
-cd ..
-
-jar cfm PARENTPOINT_SiapPakai.jar manifest.txt -C temp_build .
-
-echo === 3. Membuat ZIP Release ===
-:: if not exist Releases mkdir Releases
-:: del /F /Q Releases\PARENTPOINT_Release.zip 2>nul
-:: powershell -Command "Compress-Archive -Path PARENTPOINT_SiapPakai.jar, lib, parentpoint_db.sql -DestinationPath Releases\PARENTPOINT_Release.zip -Force"
+cd build\classes
+jar cfm ..\..\APP_Release\PARENTPOINT.jar ..\..\manifest_safe.txt *
+cd ..\..
 
 echo === SELESAI ===
-echo File PARENTPOINT_SiapPakai.jar dan Releases\PARENTPOINT_Release.zip sudah siap!
+echo File aplikasi sudah dibuat di dalam folder APP_Release!
